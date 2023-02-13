d0b58af0929ee5663f1ecc2ffc9783174e2cf97e
kinathlete <kintscher-marcel@t-online.de>
2023-02-13 18:16:09 +0100

    stacked bar chart + other stuff

--- pgc_estimator_csv.py
+++ pgc_estimator_csv.py
@@ -5,6 +5,8 @@ import os
 import pandas as pd
 # Streamlit
 import streamlit as st
+# Altair
+import altair as alt
 # App Core Functions
 # from core.exivity_export import ExivityExport
 
@@ -61,7 +63,9 @@ def build_clean_cu_pd(raw_pd):
     subset_pd = subset_pd[subset_pd['service_key'].str.contains("BoxUsage")]
     # EXTRACT REGION INTO SEPARATE COLUMN
     subset_pd['data_center'] = subset_pd['service_key'].str.split(' ', 1).str[-1]
+    subset_pd['data_center'] = "aws-" + subset_pd['data_center'].astype(str)
     subset_pd['region'] = subset_pd['service_key'].str.split(' ', 1).str[-1].str[:-1]
+    subset_pd['region'] = "aws-" + subset_pd['region'].astype(str)
     
     return subset_pd
 
@@ -78,6 +82,15 @@ def build_agg_e_by_dim(full_e_pd, dim, agg):
     agg_e_pd_by_dim = full_e_pd[[dim, 'co2_e']].groupby(dim, as_index=False).agg(agg)
     
     return agg_e_pd_by_dim
+
+# BUILD AGG EMISSIONS DF BY MULTIPLE GIVEN DIMS ('day', <some_dim>)
+def build_agg_e_by_mult_dims(full_e_pd, dims, agg):
+    dims_for_ds = list(('day',) + dims + ('co2_e',))
+    dims_for_groupby = list(('day',) + dims)
+    agg_e_pd_by_mult_dims = full_e_pd[dims_for_ds].groupby(dims_for_groupby, as_index=False).agg(agg)
+    for d in dims:
+        agg_e_pd_by_mult_dims[d]=agg_e_pd_by_mult_dims[d].astype(str)
+    return agg_e_pd_by_mult_dims
 
 # GET PUE OF DC SELECTED IN SIM FILTERS
 def get_sim_dc_pue(reg_pd):
@@ -117,6 +130,8 @@ with st.expander("Find out more about our approach - click here"):
 raw_cu_pd = import_cloud_usage_data()
 raw_reg_pd = import_regions_data()
 clean_cu_pd = build_clean_cu_pd(raw_cu_pd)
+co2t_price = 96.5
+filter_dimensions = ['service_id', 'data_center', 'region', 'account_name']
 
 # SIDEBAR
 ## FILTERS
@@ -133,14 +148,14 @@ st.sidebar.text('')
 st.sidebar.subheader('SCENARIOS')
 ### SCENARIO SETTER 1 ###
 st.sidebar.markdown("**Set your scenario 1:** :point_down:")
-sim_replaced_dc = st.sidebar.selectbox(label="Data Center to be replaced",options=filtered_em_pd['region'].unique(),\
+sim_replaced_dc = st.sidebar.selectbox(label="Data center region to be replaced",options=filtered_em_pd['region'].unique(),\
     help="Select a data center that you would like to swap out with another data center",)
-sim_replacing_dc = st.sidebar.selectbox(label="Replacing data center",options=raw_reg_pd["region"],\
+sim_replacing_dc = st.sidebar.selectbox(label="Replacing data center region",options=raw_reg_pd["region"],\
     help="Select a data center that you would like to replace the above selected data center with",)
 st.sidebar.text('')
 ### SCENARIO SETTER 2 ###
 st.sidebar.markdown("**Set your scenario 2:** :point_down:")
-sim_improved_dc = st.sidebar.selectbox(label="Data Center to be improved",options=filtered_em_pd['region'].unique(),\
+sim_improved_dc = st.sidebar.selectbox(label="Data center to be improved",options=filtered_em_pd['region'].unique(),\
     help="Select a data center where you would like to simulate a PUE factor improvement",)
 sim_pue = st.sidebar.slider(label="Set a target PUE", min_value=1.01, max_value=1.99, value=get_sim_dc_pue(raw_reg_pd),\
     step=0.01, help="Select the PUE that you want to simulate with")
@@ -152,31 +167,48 @@ if sim_run_button:
     sim_2_pd = build_filtered_sim_2_pd(clean_cu_pd, sim_improved_dc, sim_pue, raw_reg_pd)
 
 with st.container():
-    st.subheader("Selected Dataset")
+    st.subheader("Loaded Dataset (BETA)")
+    st.markdown("_Importable via Exivity API | BETA: loaded from CSV_")
     col11, col12, col13, col14 = st.columns(4)
     with col11:
-        st.markdown(":point_right: Services Used: **{}**".format(filtered_em_pd['rate_id'].count()))
+        st.metric(label="Services Used", value=filtered_em_pd['instance_value'].count(),help="Service = distinct server instance used from the AWS Elastic Compute Cloud library")
     with col12:
-        st.markdown(":point_right: Products Serviced: **{}**".format(filtered_em_pd['service_id'].nunique()))
+        st.metric(label="Products Serviced", value=filtered_em_pd['service_id'].nunique(),help="Product = a distinct product/project for which a cloud resource is utilised")
     with col13:
-        st.markdown(":point_right: Data Centers: **{}**".format(filtered_em_pd['data_center'].nunique()))
+        st.metric(label="Data Centers", value=filtered_em_pd['data_center'].nunique(),help="Data Center = a distinct building stacked with servers (in aws, a data center is \
+            determind by the last character in the location name, such as eu-central-1a<--)")
     with col14:
-        st.markdown(":point_right: Duration (d): **{}**".format(filtered_em_pd['day'].nunique()))
+        st.metric(label="Duration", value="{} days".format(filtered_em_pd['day'].nunique()),help="= number of available days in the loaded dataset")
     with st.expander("Click here to visit the input data."):
         st.markdown("**CLOUD USAGE DATA (EXIVITY)**")
-        st.dataframe(data=raw_cu_pd, use_container_width=True)
+        st.dataframe(data=filtered_em_pd, use_container_width=True)
         st.markdown("**REGIONS DATA (CLAUDY)** - limited to AWS.")
         st.dataframe(data=raw_reg_pd, use_container_width=True)
 
-
 with st.container():
-    st.subheader("CO2 Emissions Over Time (t)")
-    # st.dataframe(build_agg_e_by_dim(full_em_pd, 'day', 'sum'))
-    st.line_chart(data=build_agg_e_by_dim(filtered_em_pd, 'day', 'sum'), x='day', y='co2_e', use_container_width=True)
-
-with st.container():
-    col21, col22, col23 = st.columns(3)
+    st.subheader("ACME's Cloud Carbon Footprint")
+    col21, col22 = st.columns([1,3])
     with col21:
+        st.metric("CO2 in tons", "{}t".format(filtered_em_pd['co2_e'].sum().round(decimals=2)),\
+            help="This shows your overall carbon emissions in tons based on the select cloud usage data.")
+        st.metric("Current price per ton", "{}€".format(co2t_price),\
+            help="This shows the official EU Carbon Permit price per CO2 metric ton as per FEB 13 2023.")
+        st.metric("Offset cost total", "{}€".format(filtered_em_pd['co2_e'].sum().round(decimals=2)*co2t_price),\
+            help="This shows your total cost to offset your current carbon emissions at the current price per CO2 ton.")
+    with col22:
+        param_total_e_chart_dim = st.selectbox(label="Drill-down dimension",options=filter_dimensions,\
+    help="Select a dimension to drill-down emissions over time in the chart below",)
+        total_e_barchart = alt.Chart(build_agg_e_by_mult_dims(filtered_em_pd, (param_total_e_chart_dim,), 'sum'))\
+            .mark_bar().encode(
+                x='day',
+                y='co2_e',
+                color=param_total_e_chart_dim
+            )
+        st.altair_chart(total_e_barchart, use_container_width=True, theme="streamlit")
+
+with st.container():
+    col31, col32, col33 = st.columns(3)
+    with col31:
         st.subheader("Current State")
         st.metric("CO2 Emissions", "{}t".format(filtered_em_pd['co2_e'].sum().round(decimals=2)))
         st.write('')
@@ -185,7 +217,7 @@ with st.container():
         st.write("CO2 by Region")
         st.bar_chart(data=build_agg_e_by_dim(filtered_em_pd, 'region', 'sum'), x="region", y="co2_e", use_container_width=True)
 
-    with col22:
+    with col32:
         st.subheader("Scenario 1")
         if sim_run_button:
             st.metric("CO2 Emissions", "{}t".format(sim_1_pd['co2_e'].sum().round(decimals=2)),(sim_1_pd['co2_e'].sum()\
@@ -195,7 +227,7 @@ with st.container():
             st.write("CO2 by Region")
             st.bar_chart(data=build_agg_e_by_dim(sim_1_pd, 'region', 'sum'), x="region", y="co2_e", use_container_width=True)
 
-    with col23:
+    with col33:
         st.subheader("Scenario 2")
         if sim_run_button:
             st.metric("CO2 Emissions", "{}t".format(sim_2_pd['co2_e'].sum().round(decimals=2)),(sim_2_pd['co2_e'].sum()\
